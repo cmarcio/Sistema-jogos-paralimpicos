ceea39b2514ecdd90f0f7003ea0e6813b9384cc0
marciodscampos <marciodscampos@gmail.com>
2016-06-09 03:32:34 -0300

    Barra de menus adicionada

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Jogos Paralímpicos");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
