16d01bba985dd9740a170adc6223f9939886584d
marciodscampos <marciodscampos@gmail.com>
2016-06-10 03:37:38 -0300

    Janela pronta

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +13,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
         primaryStage.setTitle("Jogos Paralímpicos");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../resources/icons/jogos_logo.png")));
         primaryStage.show();
     }
 
